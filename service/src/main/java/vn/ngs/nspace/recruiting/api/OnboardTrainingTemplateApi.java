3c82488af8d5753a1d05a6d2b6443147801b5dce
phatnt0607 <77091353+phatnt0607@users.noreply.github.com>
2022-02-09 17:04:35 +0700

    #commit update onboartrainning Template

--- service/src/main/java/vn/ngs/nspace/recruiting/api/OnboardTrainingTemplateApi.java
+++ service/src/main/java/vn/ngs/nspace/recruiting/api/OnboardTrainingTemplateApi.java
@@ -18,13 +18,13 @@ import vn.ngs.nspace.lib.dto.BaseResponse;
 import vn.ngs.nspace.lib.utils.ResponseUtils;
 import vn.ngs.nspace.policy.utils.Permission;
 import vn.ngs.nspace.recruiting.model.OnboardTrainingTemplate;
-import vn.ngs.nspace.recruiting.model.ProfileCheckListTemplate;
 import vn.ngs.nspace.recruiting.repo.OnboardTrainingTemplateRepo;
 import vn.ngs.nspace.recruiting.service.OnboardTrainingTemplateService;
 import vn.ngs.nspace.recruiting.share.dto.OnboardTrainingTemplateDTO;
 import vn.ngs.nspace.recruiting.share.dto.ProfileCheckListTemplateDTO;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -44,16 +44,16 @@ public class OnboardTrainingTemplateApi {
     @ActionMapping(action = Permission.CREATE)
     @Operation(summary = "Create onboardTranning template"
             , description = "API for create onboardTranning template"
-            , tags = { "TemplateConfig" }
+            , tags = {"TemplateConfig"}
     )
     @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
             , schema = @Schema(implementation = String.class))
     protected ResponseEntity create(
-            @Parameter(description="ID of company")
+            @Parameter(description = "ID of company")
             @RequestHeader("cid") long cid
-            , @Parameter(description="ID of company")
+            , @Parameter(description = "ID of company")
             @RequestHeader("uid") String uid
-            , @Parameter(description="Payload DTO to create")  @RequestBody List<OnboardTrainingTemplateDTO> dtos) {
+            , @Parameter(description = "Payload DTO to create") @RequestBody List<OnboardTrainingTemplateDTO> dtos) {
         try {
             return ResponseUtils.handlerSuccess(_service.create(cid, uid, dtos));
         } catch (Exception ex) {
@@ -65,16 +65,16 @@ public class OnboardTrainingTemplateApi {
     @ActionMapping(action = Permission.UPDATE)
     @Operation(summary = "Update onboardTranning template"
             , description = "API for update onboardTranning template"
-            , tags = { "TemplateConfig" }
+            , tags = {"TemplateConfig"}
     )
     @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
             , schema = @Schema(implementation = String.class))
     protected ResponseEntity update(
-            @Parameter(description="ID of company")
+            @Parameter(description = "ID of company")
             @RequestHeader("cid") long cid
-            , @Parameter(description="ID of company")
+            , @Parameter(description = "ID of company")
             @RequestHeader("uid") String uid
-            , @Parameter(description="param in path")  @PathVariable(value = "id") Long id
+            , @Parameter(description = "param in path") @PathVariable(value = "id") Long id
             , @RequestBody OnboardTrainingTemplateDTO dto) {
         try {
             return ResponseUtils.handlerSuccess(_service.update(cid, uid, id, dto));
@@ -86,20 +86,20 @@ public class OnboardTrainingTemplateApi {
     @PostMapping("/search")
     @ActionMapping(action = Permission.VIEW)
     @Operation(summary = "Search all onboardTranning template"
-            , description = "Profile search by %name% format"
-            , tags = { "TemplateConfig" }
+            , description = "OnboardTranning search by %name% format"
+            , tags = {"TemplateConfig"}
     )
     @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
             , schema = @Schema(implementation = String.class))
     protected ResponseEntity search(
-            @Parameter(description="ID of company")
+            @Parameter(description = "ID of company")
             @RequestHeader("cid") long cid
-            , @Parameter(description="ID of company")
+            , @Parameter(description = "ID of company")
             @RequestHeader("uid") String uid
-            , @Parameter(description="Payload to search with positionId, titleId, contractTypeId")
+            , @Parameter(description = "Payload to search with positionId, titleId, contractTypeId")
             @RequestBody Map<String, Object> condition
             , Pageable pageable) {
-        try{
+        try {
             Long positionId = MapUtils.getLong(condition, "positionId", -1l);
             Long titleId = MapUtils.getLong(condition, "titleId", -1l);
             Page<OnboardTrainingTemplate> page = _reppo.search(cid, positionId, titleId, pageable);
@@ -114,26 +114,53 @@ public class OnboardTrainingTemplateApi {
     @ActionMapping(action = Permission.VIEW)
     @Operation(summary = "Get onboardTranning by Id"
             , description = "API for get onboardTranning template by Id"
-            , tags = { "TemplateConfig" }
+            , tags = {"TemplateConfig"}
             , responses = {
             @ApiResponse(description = "onboardTranning with id is response OK Wrap in BaseResponse"
                     , content = @Content(mediaType = "application/json"
-                    , schema = @Schema(implementation = BaseResponse.class ))),
+                    , schema = @Schema(implementation = BaseResponse.class))),
             @ApiResponse(content = @Content(mediaType = "application/json"
                     , schema = @Schema(implementation = ProfileCheckListTemplateDTO.class))
                     , responseCode = "200"
-                    , description = "success" )})
+                    , description = "success")})
     @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
             , schema = @Schema(implementation = String.class))
     protected ResponseEntity getById(
-            @Parameter(description="ID of company")
+            @Parameter(description = "ID of company")
             @RequestHeader("cid") long cid
-            , @Parameter(description="ID of company")
+            , @Parameter(description = "ID of company")
             @RequestHeader("uid") String uid
-            , @Parameter(description="param in path") @PathVariable(value = "id") Long id) {
+            , @Parameter(description = "param in path") @PathVariable(value = "id") Long id) {
         try {
             OnboardTrainingTemplate template = _reppo.findByCompanyIdAndId(cid, id).orElse(new OnboardTrainingTemplate());
             return ResponseUtils.handlerSuccess(_service.toDTOs(cid, uid, Collections.singletonList(template)));
+        } catch (Exception ex) {
+            return ResponseUtils.handlerException(ex);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    @ActionMapping(action = Permission.DELETE)
+    @Operation(summary = "delete all onboardTranning template"
+            , description = "onboardTranning delete by id"
+            , tags = {"TemplateConfig"}
+    )
+    @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
+            , schema = @Schema(implementation = String.class))
+    protected ResponseEntity delete(
+            @Parameter(description = "ID of company")
+            @RequestHeader("cid") long cid
+            , @Parameter(description = "ID of company")
+            @RequestHeader("uid") String uid
+            , @Parameter(description = "Payload to search with positionId, titleId, contractTypeId")
+            @RequestBody List<OnboardTrainingTemplateDTO> lstDTOs) {
+        try {
+            HashSet<Long> listId = new HashSet<>();
+            for (OnboardTrainingTemplateDTO dto : lstDTOs) {
+                listId.add(dto.getId());
+            }
+            _reppo.deleteAllByIdIn(listId);
+            return ResponseUtils.handlerSuccess();
         } catch (Exception ex) {
             return ResponseUtils.handlerException(ex);
         }
