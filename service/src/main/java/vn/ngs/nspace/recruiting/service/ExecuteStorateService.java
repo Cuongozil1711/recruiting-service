8e4eaac0ce1ae9c77e4d0fa6b2c4f2936604b2b5
hoangkimloi <h.k.loi1807@gmail.com>
2022-04-01 16:31:02 +0700

    executeStoratefix

--- service/src/main/java/vn/ngs/nspace/recruiting/service/ExecuteStorateService.java
+++ service/src/main/java/vn/ngs/nspace/recruiting/service/ExecuteStorateService.java
@@ -3,6 +3,7 @@ package vn.ngs.nspace.recruiting.service;
 import io.vertx.core.buffer.Buffer;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +14,19 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import vn.ngs.nspace.lib.dto.BaseResponse;
 import vn.ngs.nspace.lib.exceptions.BusinessException;
+import vn.ngs.nspace.lib.utils.HttpUtils;
+import vn.ngs.nspace.recruiting.command.GroupCreateCommand;
+import vn.ngs.nspace.recruiting.command.InitFolderCommand;
+import vn.ngs.nspace.recruiting.dto.FolderDTO;
+import vn.ngs.nspace.recruiting.dto.GroupDTO;
+import vn.ngs.nspace.recruiting.dto.UploadTemplateDTO;
+import vn.ngs.nspace.recruiting.share.dto.utils.Constants;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Component
 public class ExecuteStorateService {
@@ -22,6 +34,8 @@ public class ExecuteStorateService {
     public String StorageServiceURL;
     @Value("${nspace.service.storage.key:nothing}")
     public String StorageServiceKey;
+    @Value("${nspace.service.user.URL}")
+    public String UserServiceURL;
 
     protected HttpHeaders createHeader(String userId, Long companyId) {
         final HttpHeaders headers = new HttpHeaders();
@@ -39,15 +53,71 @@ public class ExecuteStorateService {
         HttpEntity<ResponseEntity> request = new HttpEntity<>(headers);
         return request;
     }
-    public void uploadFile( MultipartFile file) throws RuntimeException {
+    public GroupDTO createGroup(String requestUserId, long companyId, Long parentId
+            , String code, String type, String name, String description) throws Exception {
+        URI uri = new URI(UserServiceURL + "/api/group/create");
+        HttpMethod method = HttpMethod.POST;
+        RestTemplate restTemplate = new RestTemplate();
+
+        GroupCreateCommand cmd = new GroupCreateCommand();
+        cmd.setCompanyId(companyId);
+        cmd.setUserId(requestUserId);
+        cmd.setCode(code);
+        cmd.setType(type);
+        cmd.setName(name);
+        cmd.setParentId(parentId);
+        cmd.setLeaderId(0L);
+        cmd.setDescription(description);
+
+        HttpHeaders headers = createHeader(requestUserId, companyId);
+        HttpEntity request = new HttpEntity<>(cmd, headers);
+        ParameterizedTypeReference<BaseResponse<GroupDTO>> responseType = new ParameterizedTypeReference<BaseResponse<GroupDTO>>() {
+        };
+        BaseResponse<GroupDTO> response = null;
+        ResponseEntity res = restTemplate.exchange(uri, method, request, responseType);
+        response = (BaseResponse<GroupDTO>) res.getBody();
+
+        return response.getData();
+    }
+    public List<FolderDTO> createFolder(String requestUserId, long companyId, List<String> groupPaths,
+                                        List<String> groupPathIds, List<String> names, List<Long> gids) throws Exception {
+        HttpHeaders headers = createHeader(requestUserId, companyId);
+        InitFolderCommand cmd = new InitFolderCommand();
+        cmd.setApplication(Constants.REFERENCE_GROUP_SERVICE_APPLICATION);
+        cmd.setNames(names);
+        cmd.setGids(gids);
+        cmd.setGroupPaths(groupPaths);
+        cmd.setGroupPathIds(groupPathIds);
+
+        HttpEntity request = new HttpEntity<>(cmd, headers);
+        String uri = StorageServiceURL + "/folder/init";
+        BaseResponse<FolderDTO[]> response = HttpUtils.internalRequest(uri, HttpMethod.POST, request, FolderDTO[].class);
+        return Arrays.asList(response.getData());
+    }
+    public void uploadFile(String requestUserId, long companyId, MultipartFile file) throws RuntimeException {
         try {
 
-            URI uri = new URI(StorageServiceURL + "/file/upload/8aaa81f77fde9ba7017fdef1e13d000e");
+
+            String nameText = "abcxyz";
+            GroupDTO group = createGroup(requestUserId, companyId,
+                    Constants.DEFAULT_PARENT_GROUP_ID, UUID.randomUUID().toString()
+                    , nameText
+                    , Constants.GROUP_NAME_RECRUITING, Constants.GROUP_DESCRIPTION_RECRUITING);
+            //create group
+            List<String> groupPathIds = Collections.singletonList(group.getId().toString());
+            List<Long> gids = Collections.singletonList(group.getId());
+            List<String> names = Collections.singletonList(group.getName());
+            List<String> groupPaths = Collections.singletonList(group.getName());
+
+
+            List<FolderDTO> folders = createFolder(requestUserId, companyId,
+                    groupPaths, groupPathIds, names, gids);
+            String folderId = folders.get(0).getId();
+            URI uri = new URI(StorageServiceURL + "/file/upload"+folderId);
             HttpMethod method = HttpMethod.POST;
             RestTemplate restTemplate = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
+            HttpHeaders headers = createHeader(requestUserId,companyId);
             headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
             MultiValueMap body
                     = new LinkedMultiValueMap<>();
             body.add("files", file.getResource());
@@ -56,7 +126,8 @@ public class ExecuteStorateService {
                     = new HttpEntity<>(body, headers);
 
             ResponseEntity<String> response = restTemplate
-                    .postForEntity(uri, requestEntity, String.class);
+                    .postForEntity(uri,requestEntity, String.class);
+
 
         } catch (Exception e){
             BaseResponse baseResponse = Buffer.buffer(((HttpClientErrorException.BadRequest) e).getResponseBodyAsByteArray()).toJsonObject().mapTo(BaseResponse.class);
