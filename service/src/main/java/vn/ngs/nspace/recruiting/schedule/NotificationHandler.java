900feafd8b450c50dd9ff69dfd83a9f1229812ec
minhld1007 <minhld10071984@gmail.com>
2022-04-06 15:52:16 +0700

    email schedule

---
+++ service/src/main/java/vn/ngs/nspace/recruiting/schedule/NotificationHandler.java
@@ -0,0 +1,24 @@
+package vn.ngs.nspace.recruiting.schedule;
+
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class NotificationHandler implements Handler {
+
+    public NotificationHandler() {
+    }
+
+    @Override
+    public void process(Map<String, Object> data) {
+        try {
+            ScheduleTaskCommand scheduleDTO = Json.decodeValue(new JsonObject(data).toBuffer(), ScheduleTaskCommand.class);
+            //service.remindTaskNotices(scheduleDTO.getCompanyId(), scheduleDTO.getTaskId(), scheduleDTO.getEvent());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
