53422fd26d0baecdc1b16f6b706fa52d41012b48
toianh99 <toileanh84@gmail.com>
2022-05-17 08:50:28 +0700

    update api candidate cv

--- service/src/main/java/vn/ngs/nspace/recruiting/api/v2/CandidateV2Api.java
+++ service/src/main/java/vn/ngs/nspace/recruiting/api/v2/CandidateV2Api.java
@@ -19,6 +19,8 @@ import vn.ngs.nspace.recruiting.service.v2.CandidateV2Service;
 import vn.ngs.nspace.recruiting.share.dto.CandidateDTO;
 import vn.ngs.nspace.recruiting.share.request.CandidateFilterRequest;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("v2/candidate")
 @RequiredArgsConstructor
@@ -42,7 +44,7 @@ public class CandidateV2Api {
             , Pageable page
     ) {
         try {
-            Page<CandidateDTO> candidateDTOPage = candidateService.getPage(uid,cid, request, page);
+            Page<CandidateDTO> candidateDTOPage = candidateService.getPage(uid, cid, request, page);
 
             return ResponseUtils.handlerSuccess(candidateDTOPage);
         } catch (Exception e) {
@@ -52,11 +54,11 @@ public class CandidateV2Api {
 
     @GetMapping("/count")
     public ResponseEntity getCountAll(
-            @Parameter(description="ID of company")
+            @Parameter(description = "ID of company")
             @RequestHeader("cid") long cid
-            , @Parameter(description="ID of company")
+            , @Parameter(description = "ID of company")
             @RequestHeader("uid") String uid
-    ){
+    ) {
         try {
             return ResponseUtils.handlerSuccess(candidateService.getCount(cid));
         } catch (Exception ex) {
@@ -118,6 +120,84 @@ public class CandidateV2Api {
             , @RequestBody CandidateDTO dto) {
         try {
             return ResponseUtils.handlerSuccess(candidateService.create(cid, uid, dto));
+        } catch (Exception ex) {
+            return ResponseUtils.handlerException(ex);
+        }
+    }
+
+    @GetMapping("{id}")
+    @ActionMapping(action = Permission.VIEW)
+    @Operation(summary = "Get Candidate by Id"
+            , description = "Get Candidate by Id"
+            , tags = {"Candidate"}
+    )
+    @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
+            , schema = @Schema(implementation = String.class))
+    protected ResponseEntity getById(
+            @Parameter(description = "Id of Company") @RequestHeader Long cid
+            , @Parameter(description = "Id of User") @RequestHeader String uid
+            , @Parameter(description = "Id of record") @PathVariable(value = "id") Long id) {
+        try {
+            CandidateDTO candidateDTO = candidateService.getById(uid, cid, id);
+            return ResponseUtils.handlerSuccess(candidateDTO);
+        } catch (Exception ex) {
+            return ResponseUtils.handlerException(ex);
+        }
+    }
+
+    @PutMapping("{id}")
+    @ActionMapping(action = Permission.UPDATE)
+    @Operation(summary = "Update Candidate by Id"
+            , description = "Update Candidate by Id"
+            , tags = {"Candidate"}
+    )
+    @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
+            , schema = @Schema(implementation = String.class))
+    protected ResponseEntity updateById(
+            @Parameter(description = "Id of Company") @RequestHeader Long cid
+            , @Parameter(description = "Id of User") @RequestHeader String uid
+            , @Parameter(description = "Id of record") @PathVariable(value = "id") Long id
+            , @RequestBody CandidateDTO dto) {
+        try {
+            return ResponseUtils.handlerSuccess(candidateService.update(cid, uid, id, dto));
+        } catch (Exception ex) {
+            return ResponseUtils.handlerException(ex);
+        }
+    }
+
+    @PostMapping()
+    @ActionMapping(action = Permission.CREATE)
+    @Operation(summary = "Create single Candidate"
+            , description = "Create single Candidate"
+            , tags = {"Candidate"}
+    )
+    @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
+            , schema = @Schema(implementation = String.class))
+    private ResponseEntity create(
+            @Parameter(description = "Id of Company") @RequestHeader Long cid
+            , @Parameter(description = "Id of User") @RequestHeader String uid
+            , @RequestBody CandidateDTO dto) {
+        try {
+            return ResponseUtils.handlerSuccess(candidateService.create(cid, uid, dto));
+        } catch (Exception ex) {
+            return ResponseUtils.handlerException(ex);
+        }
+    }
+
+    @PostMapping("/creates")
+    @ActionMapping(action = Permission.CREATE)
+    @Operation(summary = "Create list Candidate"
+            , description = "Create list Candidate"
+            , tags = {"Candidate"}
+    )
+    @Parameter(in = ParameterIn.HEADER, description = "Addition Key to bypass authen", name = "key"
+            , schema = @Schema(implementation = String.class))
+    private ResponseEntity create(
+            @Parameter(description = "Id of Company") @RequestHeader Long cid
+            , @Parameter(description = "Id of User") @RequestHeader String uid
+            , @Parameter(description = "List of candidate") @RequestBody List<CandidateDTO> dtos) {
+        try {
+            return ResponseUtils.handlerSuccess(candidateService.create(cid, uid, dtos));
         } catch (Exception ex) {
             return ResponseUtils.handlerException(ex);
         }
